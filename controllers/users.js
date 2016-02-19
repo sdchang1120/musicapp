28db18aff1fa1b778e3b629e7f341141395fd261
Sharon Chang <changsharond@gmail.com>
2016-02-19 13:58:16 -0500

    updating

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/users.js').User;
-
+// var User = require('../models/users.js').User;
 
 module.exports = router;
