78ff1d216c0d41656cb35f2401263b8cbec15e04
Sharon Chang <changsharond@gmail.com>
2016-02-19 16:38:29 -0500

    setting up controllers

--- controllers/users.js
+++ controllers/users.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 // var User = require('../models/users.js').User;
 
+router.get('/', function(req, res) {
+  res.render('index.ejs');
+})
+
 module.exports = router;
