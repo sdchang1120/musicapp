ef067823c32e4d7e913386fdc515aa0806dea22d
Sharon Chang <changsharond@gmail.com>
2016-02-19 19:08:16 -0500

    commenting out password hashing and validating

--- models/users.js
+++ models/users.js
@@ -12,6 +12,15 @@ var userSchema = mongoose.Schema({
   // playlist: [musicSchema];
 })
 
+// model method - hashing password
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
 var User = mongoose.model('User', userSchema);
 
 // EXPORT USER
