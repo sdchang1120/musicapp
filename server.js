8243ac5025ebb5ade70e57a02e2d09338aa80b2c
Sharon Chang <changsharond@gmail.com>
2016-02-21 18:05:47 -0500

    setting up middleware and controllers

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+// ====================
 // REQUIREMENTS
+// ====================
 var express        = require('express'),
     app            = express(),
     bodyParser     = require('body-parser'),
     methodOverride = require('method-override'),
-    // mongoose  = require('mongoose'),
+    mongoose  = require('mongoose'),
+    passport       = require('passport'),
+    session        = require('express-session'),
     port           = 3000 || process.env.PORT;
+
+// CONNECT TO MONGO DB MUSIC_APP
+mongoose.connect('mongodb://localhost/music_app');
+
+require('./config/passport.js')(passport) // pass possport for configuration
 
 // SET UP STATIC PUBLIC FILES
 app.use(express.static('public'));
@@ -22,25 +31,29 @@ app.use(methodOverride(function(req, res){
   }
 }));
 
+// PASSPORT REQUIREMENT
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret; shows cookies
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 // CONTROLLERS
 var usersController = require('./controllers/users.js');
 app.use('/users', usersController);
-
 var musicController = require('./controllers/music.js');
 app.use('/music', musicController);
 
+// INDEX PAGE
 app.get('/', function(req, res) {
-	res.render('index.ejs');
+	res.redirect('/users');
 });
 
-// CONNECT TO MONGOOSE
-// mongoose.connect('mongodb://localhost/maps_models');
-
+// ====================
 // LISTEN ON PORT 3000
-// mongoose.connection.once('open', function() {
+// ====================
+mongoose.connection.once('open', function() {
   app.listen(port, function() {
     console.log('====================');
     console.log('Running on port ' + port);
     console.log('====================');
   });
-// })
+});
