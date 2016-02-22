1a7fd281ce09bbc89c618cb624e7f9235e297e8b
Sharon Chang <changsharond@gmail.com>
2016-02-21 21:21:41 -0500

    updating users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/users.js').User;
+var Playlist = require('../models/playlist.js').Playlist;
 // var Music = require('../models/music.js').Music;
 var passport = require('passport');
 
@@ -21,6 +22,13 @@ router.get('/json', function(req, res) {
   });
 });
 
+// SIGNUP
+router.post('/', passport.authenticate('local-signup', {
+  failureRedirect : '/users' // redirect back to the signup page if there is an error
+}), function(req, res) {
+  res.redirect('/users/'+req.user.id); // req.user can be called bc of passport.js
+});
+
 // LOGIN
 router.post('/login', passport.authenticate('local-login', {failureRedirect : '/users'}), function(req, res) {
   // console.log(req.user.id);
@@ -38,17 +46,42 @@ router.get('/:id', isLoggedIn, function(req, res) {
   req.params.id == req.user.id ? res.locals.usertrue = true : res.locals.usertrue = false;
   // find user ID
   User.findById(req.params.id, function(err, user) {
+    // console.log(user.id);
     // render user's show page
     res.render('users/show.ejs', user);
   });
 });
 
-// SIGNUP
-router.post('/', passport.authenticate('local-signup', {
-  failureRedirect : '/users' // redirect back to the signup page if there is an error
-}), function(req, res) {
-  res.redirect('/users/'+req.user.id); // req.user can be called bc of passport.js
-});
+router.get('/:id/new', function(req, res) {
+  var id = req.params.id;
+  // console.log(id);
+  User.findById(id, function(err, user) {
+    // console.log(user.id)
+    res.render('users/newplaylist.ejs', user);
+  })
+})
+
+router.get('/:id/newlist/newsong', function(req, res) {
+  var id = req.params.id;
+  var newSong = new Music(req.body);
+  newSong.save(function(err, song) {
+    console.log(song);
+    User.findByIdAndUpdate(id, {$push: {music: song}}, {new: true}, function(err) {
+      res.redirect('/users/' + id)
+    })
+  })
+})
+
+router.post('/:id/newlist', function(req, res) {
+  var id = req.params.id;
+  var newPlaylist = new Playlist(req.body);
+  newPlaylist.save(function(err, playlist) {
+    console.log(playlist);
+    User.findByIdAndUpdate(id, {$push: {playlist: newPlaylist}}, {new: true}, function(err) {
+      res.redirect('/users/' + id + '/newlist/newsong');
+    });
+  })
+})
 
 // isLoggedIn function
 // route middleware to make sure a user is logged in
