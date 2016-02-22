269c1611ae1bad7687de2c5d3b291f4d55b2d6df
Sharon Chang <changsharond@gmail.com>
2016-02-21 21:22:26 -0500

    adding playlist_title key

--- models/music.js
+++ models/music.js
@@ -3,10 +3,11 @@ var mongoose = require('mongoose');
 
 // SETTING UP MUSIC SCHEMA
 var musicSchema = mongoose.Schema({
-  playlist_title: String,
   song_title: String,
   artist: String,
   music_link: String
 })
+
+var Music = mongoose.model('Music', musicSchema);
 
 module.exports.Music = Music;
