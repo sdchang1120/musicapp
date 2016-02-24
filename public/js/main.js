e2460af3603ed172e8afbe6ed43abad3527a3f58
Sharon Chang <changsharond@gmail.com>
2016-02-24 14:16:54 -0500

    attempting to dynamically change td width

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,12 @@ $(function() {
   $userPlaylist = $('.user-playlist');
   $viewUserPlaylist = $('.view-user-playlist');
 
+  $td = $('td');
+  $trLength = $('tr').children.length;
+  console.log($trLength);
+
+  $td.css('width', 'calc(100%/5)');
+
   // $userPlaylist.hover(function() {$viewUserPlaylist.show()}, function() {$viewUserPlaylist.hide()});
 
 });
