be522311f5fd4bd3e2b8e82370c16b29e456ffb3
Sharon Chang <changsharond@gmail.com>
2016-02-24 19:35:00 -0500

    adding music/json route

--- controllers/music.js
+++ controllers/music.js
@@ -1,4 +1,24 @@
+// ====================
+//     REQUIREMENTS
+// ====================
 var express = require('express');
 var router = express.Router();
+var Music = require('../models/music.js').Music;
+
+// ====================
+//        INDEX
+// ====================
+
+// ALL USERS
+router.get('/', function(req, res) {
+    res.redirect('/music/json');
+});
+
+// ALL USERS - JSON
+router.get('/json', function(req, res) {
+  Music.find(function(err, music) {
+    res.send(music);
+  });
+});
 
 module.exports = router;
